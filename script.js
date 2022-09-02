f2377d28918d3663c072d6ab681661dcee3e459f
Caiodiv <84077829+Caiodiv@users.noreply.github.com>
2022-09-02 15:33:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,224 @@
-    document.addEventListener("keypress", function(e) {
-        if(e.key === 'b' || e.key === 'B') {
-            var btn = document.querySelector("#submit");
+    document.addEventListener("keypress", function (e) {
+  if (e.key === "b" || e.key === "B") {
+    var btn = document.querySelector("#submit");
 
-            window.alert("Você não é o dinheiro perdido no bolso do casaco, mas fiquei muito feliz de te encontrar!");
-          
-          btn.click();
-        }
-      });
+    window.alert(
+      "Eu te amo, não só agora mas sempre, e sonho com o dia em que você vai me abraçar novamente."
+    );
+
+    btn.click();
+  }
+  if (e.key === "q" || e.key === "Q") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Eu te amo para amar-te e não para ser amado, pois nada me dá tanta felicidade como te ver feliz."
+    );
+
+    btn.click();
+  }
+  if (e.key === "w" || e.key === "W") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Se eu disser que eu te amo, acredite. Não sou de brincar com sentimentos, sei o quanto dói."
+    );
+
+    btn.click();
+  }
+  if (e.key === "e" || e.key === "E") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "O problema é que eu te amo. Não tenho dúvidas que com você daria certo."
+    );
+
+    btn.click();
+  }
+  if (e.key === "r" || e.key === "R") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Quando penso em você, não posso deixar de sorrir, sabendo que você me completa. Eu te amo, não só agora, mas sempre."
+    );
+
+    btn.click();
+  }
+  if (e.key === "t" || e.key === "T") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Eu odeio o quanto eu te amo, não suporto o quanto eu preciso de você."
+    );
+
+    btn.click();
+  }
+  if (e.key === "y" || e.key === "Y") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("Eu te amo em todos os universos");
+
+    btn.click();
+  }
+  if (e.key === "u" || e.key === "U") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("De brigas e problemas, passamos a ser amor e solução");
+
+    btn.click();
+  }
+  if (e.key === "i" || e.key === "I") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Eu vou fazer de tudo pra você ficar, mas é que eu vivo no meu mundo e tô sempre perdido"
+    );
+
+    btn.click();
+  }
+  if (e.key === "o" || e.key === "O") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Você me olhava com esse sorriso covarde. Joia mais exótica em qualquer detalhe"
+    );
+
+    btn.click();
+  }
+  if (e.key === "p" || e.key === "P") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "A culpa é sua cê me viciar no seu cheiro. A culpa é sua eu nunca pensar em ir embora"
+    );
+
+    btn.click();
+  }
+  if (e.key === "a" || e.key === "A") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Acho que esse teu jeito combinou com a minha vibe. Doce e delicada, mas cheia de maldade"
+    );
+
+    btn.click();
+  }
+  if (e.key === "s" || e.key === "S") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("Longe da realidade, você é minha flor de Marte");
+
+    btn.click();
+  }
+  if (e.key === "d" || e.key === "D") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("Roubei teu coração, te trouxe pro meu mundo");
+
+    btn.click();
+  }
+  if (e.key === "f" || e.key === "F") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Conheço tuas curvas na palma da mão e o seu corpo na ponta da língua"
+    );
+
+    btn.click();
+  }
+  if (e.key === "g" || e.key === "G") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Me tem na sua palma e você sabe bem. Te tenho no coração, favor não partir"
+    );
+
+    btn.click();
+  }
+  if (e.key === "h" || e.key === "H") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("O meu quarto ainda tem o seu cheiro de amor e sacanagem");
+
+    btn.click();
+  }
+  if (e.key === "j" || e.key === "J") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("Seja pra mim só o seu amor malvadin");
+
+    btn.click();
+  }
+  if (e.key === "k" || e.key === "K") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("Se quiser fechar só nós. Te prometo que eu não sumo");
+
+    btn.click();
+  }
+  if (e.key === "l" || e.key === "L") {
+    var btn = document.querySelector("#submit");
+
+    window.alert("Se quiser fechar só nós. Te prometo que eu não sumo");
+
+    btn.click();
+  }
+  if (e.key === "z" || e.key === "Z") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Eu faço o que for preciso pra te ver sorrindo. Preciso do teu sorriso pra viver sorrindo"
+    );
+
+    btn.click();
+  }
+  if (e.key === "x" || e.key === "X") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "eu sei entendo que acreditar em mim da medo, mas deixa eu te contar mais um segredo: \n" +
+        "Desvendar o amor é amar sem medo. O começo é doce o final azedo"
+    );
+
+    btn.click();
+  }
+  if (e.key === "c" || e.key === "C") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Todo mundo acha que diamante é a joia mais rara do mundo, mas só dizem isso porque não te acharam ainda."
+    );
+
+    btn.click();
+  }
+  if (e.key === "v" || e.key === "V") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "É a baixinha que consegue me deixar nas altura. Sua presença dá mais brisa do que a branca pura"
+    );
+
+    btn.click();
+  }
+  if (e.key === "n" || e.key === "N") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Meu plano A é te fazer feliz" +
+        "Meu plano B é o sucesso do plano A, nega!"
+    );
+
+    btn.click();
+  }
+  if (e.key === "m" || e.key === "M") {
+    var btn = document.querySelector("#submit");
+
+    window.alert(
+      "Agora, me encontro pensando em você. " +
+        "Cê partiu e eu já to com vontade de te ver"
+    );
+
+    btn.click();
+  }
+});
+
